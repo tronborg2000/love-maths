69f1277cd2874dce14c76825e1a1231b5ba2ae80
tronborg2000 <colinweldon1980@gmail.com>
2022-11-01 16:23:17 +0000

    add subtract function

--- assets/js/script.js
+++ assets/js/script.js
@@ -28,7 +28,9 @@ function runGame(gameType) {
         displayAdditionQuestion(num1, num2)
     } else if (gameType === "multiply") {
         displayMultiplyQuestion(num1, num2);
-    }else {
+    } else if (gameType === "subtract" ) {
+        displaySubtractQuestion(num1, num2);
+    } else {
         alert(`unknown game type: ${gameType}`)
         throw `unknown game type: ${gameType}. Aborting!`;
     }
@@ -68,7 +70,9 @@ function calculateCorrectAnswer() {
         return [operand1 + operand2, "addition"];
     } else if (operator === "x") {
         return [operand1 * operand2, "multiply"];
-    } else {
+    } else if (operator === "-") {
+        return [operand1 - operand2, "subtract"];
+    }else {
         alert(`unimplemented operator ${operator}`)
         throw `unimplemented operator ${operator}. Aborting!`;
     }
@@ -95,7 +99,10 @@ function displayAdditionQuestion(operand1, operand2) {
     document.getElementById('operator').textContent = "+";
 
 }
-function displaySubtractQuestion() {
+function displaySubtractQuestion(operand1, operand2) {
+    document.getElementById("operand1").textContent = operand1 > operand2 ? operand1 : operand2;
+    document.getElementById("operand2").textContent = operand1 > operand2 ? operand2 : operand1;
+    document.getElementById('operator').textContent = "-";
 
 }
 
