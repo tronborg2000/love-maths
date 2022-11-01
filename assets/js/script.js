911299d31be02e4945cfd733a72780b0e797b90f
tronborg2000 <colinweldon1980@gmail.com>
2022-11-01 16:58:31 +0000

    add initial division funtion

--- assets/js/script.js
+++ assets/js/script.js
@@ -40,7 +40,9 @@ function runGame(gameType) {
         displayMultiplyQuestion(num1, num2);
     } else if (gameType === "subtract" ) {
         displaySubtractQuestion(num1, num2);
-    } else {
+    } else if (gameType === "division" ) {
+        displaySubtractQuestion(num1, num2);
+        else {
         alert(`unknown game type: ${gameType}`)
         throw `unknown game type: ${gameType}. Aborting!`;
     }
@@ -82,7 +84,10 @@ function calculateCorrectAnswer() {
         return [operand1 * operand2, "multiply"];
     } else if (operator === "-") {
         return [operand1 - operand2, "subtract"];
-    }else {
+    } else if (operator === "/") {
+        return [operand1 / operand2, "division"];
+    }
+        else {
         alert(`unimplemented operator ${operator}`)
         throw `unimplemented operator ${operator}. Aborting!`;
     }
@@ -120,4 +125,10 @@ function displayMultiplyQuestion(operand1, operand2) {
     document.getElementById('operand1').textContent = operand1;
     document.getElementById('operand2').textContent = operand2;
     document.getElementById('operator').textContent = "x";
+}
+
+function displayDivisionQuestion(operand1, operand2) {
+    document.getElementById('operand1').textContent = operand1 > operand2 ? operand1 : operand2;
+    document.getElementById('operand2').textContent = operand1 > operand2 ? operand2 : operand1;
+    document.getElementById('operator').textContent = "/";
 }
