56f312d7e4ed29eba671ef42fb81d6c3ed04184b
tronborg2000 <colinweldon1980@gmail.com>
2022-11-01 16:00:56 +0000

    check answer to see that it matches answer

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,7 +4,7 @@ document.addEventListener("DOMContentLoaded", function() {
     for (let button of buttons ) {
         button.addEventListener("click", function() {
             if (this.getAttribute("data-type") === "submit") {
-                alert("You clicked Submit!");
+                checkAnswer();
             } else {
                 let gameType = this.getAttribute("data-type");
                 runGame(gameType);
@@ -31,8 +31,23 @@ function runGame(gameType) {
         throw `unknown game type: ${gameType}. Aborting!`;
     }
 }
-
+/**
+ * Checks the answer against the first element in 
+ * the returned calculatedCorrectAnswer array.
+ */
 function checkAnswer() {
+   
+    let userAnswer = parseInt(document.getElementById("answer-box").value);
+    let calculatedAnswer = calculateCorrectAnswer();
+    let isCorrect = userAnswer === calculatedAnswer[0];
+
+    if (isCorrect) {
+        alert("Hey! You got it right! :D");
+    } else {
+        alert(`Awwww.... you answered ${userAnswer}. The correct answer was ${calculatedAnswer[0]}!`);
+    }
+
+    runGame(calculatedAnswer[1]);
 
 }
 
